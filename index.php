402999c87ab456d2189cc837526c3180f81d24e8
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-06-09 01:06:22 +0200

    saving the emails to a separate file in php

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+$email = $_POST['email'] ?? null;
+
+$status = null;
+if ($email) {
+  $file = fopen('emails', 'a');
+  $result = fwrite($file, $email . "\n");
+  fclose($file);
+
+  if ($result !== false) {
+    $status = true;
+  } else {
+    $status = false;
+  }
+}
+
+?>
+
+<html>
+
+<head>
+  <style>
+    input {
+      display: inline-block;
+    }
+
+    .error {
+      color: red;
+      padding: 4px 10px;
+    }
+  </style>
+</head>
+
+<body>
+  <h1>Witam na mojej stronie</h1>
+  <h3>Zapisz się na newsletter zostawiając swój adres email</h3>
+  <?php if ($status === true) : ?>
+    <div class="message">Brawo!!! Twój email został dodany do newslettera</div>
+  <?php elseif ($status === false) : ?>
+    <div class="error">Wystąpił błąd podczas zapisu danych</div>
+  <?php endif; ?>
+  <section>
+    <form action="/" method="POST">
+      <label>Email: <input type="email" name="email" /></label>
+      <input type="submit" value="Zapisz na newsletter" />
+
+    </form>
+  </section>
+</body>
+
+</html>
